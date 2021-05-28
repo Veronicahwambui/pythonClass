fa7bafd4c00e93be4c24ecb72523ab0f9cdfd73e
veronicahwambui <njengaveronicahwambui@gmail.com>
2021-05-28 21:19:56 +0300

    second

--- student.py
+++ student.py
@@ -1,11 +1,7 @@
 class Student:
         school="AkiraChix"
 
-# student1=Student()
-# student2=Student()
-# student3=Student()
-# student1.school    
-# student2.school
+
         def __init__(self,name,age):
              self.name= name
              self.age=age
