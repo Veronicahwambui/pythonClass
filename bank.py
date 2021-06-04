6bf7c4129bebab931b0b372c511829573035a939
veronicahwambui <njengaveronicahwambui@gmail.com>
2021-06-04 20:17:22 +0300

    second

--- bank.py
+++ bank.py
@@ -1,13 +1,30 @@
-class Bank:
-    #  accounttype="Student"
-    #  pin=86779
-    #  balance=30.0
-     def __init__(self ,accounttype,pin,balance) :
-         self.accounttype=accounttype
-         self.pin=pin
-         self.balance=balance
-     def withdraw(self):
-         return f"my  account type is {self.accounttype} ,pin {self.pin}.My balance is {self.balance}"
+class Account:
+    
+     def __init__(self ,name,phone,transaction) :
+         self.name=name
+         self.phone=phone
+         self.transaction=50
+         self.balance=0
+     def deposit(self,amount):
+            if amount <=0:
+                return f"please deposit  a valid amount"
+            else:
+
+                self.balance+=amount
+                return f"Hello  {self.name} your have deposited {amount}.Your new balance is {self.balance}"
+     def  withdraw(self,withdraw ):
+            
+                 total=withdraw +self.transaction
+                    #    return f"Hello ,your{self.a}
+                 if withdraw <=0:
+                       return f" input a valid amount"
+                 elif total >=self.balance :
+                      return f"input client balance"
+                 else:
+                     self.balance=total
+                     return f" hello{self.name} you have successfully withdraw {withdraw} account balance{self.balance}"
+
+
+                    
         
-     def atmcard(self):
-          return f"my atmcard balance is {self.balance}"
+     
